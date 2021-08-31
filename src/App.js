477abdd29c4d82161bfb4b52f258c3d7c68fd9a7
Vedika Mujgule <vedika.mujgule@dataroom.nse.co.in>
2021-08-31 12:17:35 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+// import "App.css";
+// import thumb from "./thumb1.jpg";
+
+//Import Components
+// import Nav from "./Nav";
+
+const App = () => (
+  <div>
+    <Nav />
+    <h1 className="wow">Hello React!!!!!</h1>
+  </div>
+);
+
+export default App;
